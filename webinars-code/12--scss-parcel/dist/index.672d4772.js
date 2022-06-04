184bda7bdd475c38379f299328aefc2cf7d1f92a
yamnyk <yamnyk.vlog@gmail.com>
2022-06-04 11:19:56 +0300

    scss webinar completed

---
+++ webinars-code/12--scss-parcel/dist/index.672d4772.js
@@ -0,0 +1,7 @@
+window.addEventListener("resize", (e)=>{
+    const details = document.querySelector("details.navigation");
+    if (window.innerWidth >= 1024) details.open = true;
+    else details.open = false;
+});
+
+//# sourceMappingURL=index.672d4772.js.map
