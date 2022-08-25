be3ef84a3cd0095b74a08356dbfefa5746881eb3
yamnyk <yamnyk.blog@gmail.com>
2022-08-25 19:36:57 +0300

    cashflower | add login basic layout

--- webinars-code/cashflower-app/gulp/config.js
+++ webinars-code/cashflower-app/gulp/config.js
@@ -2,15 +2,16 @@ export const path = {
   root: './',
   build: {
     self: './build/',
+    html: './*.html',
     js: './build/js',
     img: './build/img',
     fonts: './build/fonts',
     css: './build/css'
   },
   html: './index.html',
-  js: './src/js/*.js',
+  js: './src/js/**/*.js',
   mainJS: './src/js/main.js',
-  scss: './src/scss/*.scss',
+  scss: './src/scss/**/*.scss',
   assets: './src/assets/**/*',
   img: './src/assets/img/**/*',
   fonts: './src/assets/fonts/**/*'
